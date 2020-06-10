9a40ee0421f6b8e385e9bf0efd26a026cb763ccd
felipetoro04 <felipe.toro2020twk.cl>
2020-06-10 15:00:21 -0400

    Create Clase 1 ejercicio 3.py

---
+++ Clase 1 ejercicio 3.py
@@ -0,0 +1,30 @@
+#tipo de datos
+
+apellido = 7
+print("apellido", type(apellido))
+
+nombre = "Felipe"
+print ("nombre", type(nombre))
+
+casado = True
+print("casado", type(casado))
+
+pi= 3.14
+print("pi", type(pi))
+
+edad = 3
+print("edad", type(edad))
+
+
+mensaje = "Mensaje para los estudiantes con comillas dobles"
+print(mensaje)
+mensaje2= 'Mensaje para los estudiantes con comillas simples'
+print(mensaje2)
+mensaje3 = '''Mensaje para los estudiantes
+ con salto de
+  linea'''
+print(mensaje3)
+mensaje4 = "Mensaje para los estudiantes" \
+ "con salto de tres\
+ lineas"
+print(mensaje4)
