69631c9fc5e105eacd47d07c45934aada89c1062
felipetoro04 <felipe.toro2020twk.cl>
2020-06-10 14:58:34 -0400

    Create Clase 1 ejercicio 1.py

---
+++ Clase 1 ejercicio 1.py
@@ -0,0 +1,23 @@
+#imprimir por pantalla el contenido
+print("hola mundo")
+
+#Variable string
+direccion = "Mackena 1234, temuco, chile"
+print(direccion)
+nombre = "Felipe"
+apellido = "Toro"
+
+#variable tipo int
+edad = 40
+
+#variable tipo Float
+peso = 89.5
+
+#Variable tipo boolean
+casado = False
+
+print("Hola", nombre, apellido)
+print("Tu edad es:", edad, "años")
+
+
+
