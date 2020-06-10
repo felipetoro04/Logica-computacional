020b6365f8eb675f693aea97a4a16fceda12a3c7
felipetoro04 <felipe.toro2020twk.cl>
2020-06-10 15:00:14 -0400

    Create Clase 1 ejercicio 2.py

---
+++ Clase 1 ejercicio 2.py
@@ -0,0 +1,20 @@
+#operadores aritmeticos
+print("Operadores Aritmeticos")
+
+suma = 10+5
+resta = 10-5
+multi = 10*5
+divi = 10/5
+modu = 10%5
+elevado = 10**5
+enteroDiv = 10//5
+
+print("La suma es:",suma)
+print("La resta es:",resta)
+print("La multiplicacion es:",multi)
+print("La division es:",divi)
+print("El modulo es:",modu)
+print("La resultado es:",elevado)
+print("La cociente de la division es:",enteroDiv)
+
+
