e8277ab20c3016b733107588e506a898f17bd2e9
felipetoro04 <felipe.toro2020twk.cl>
2020-06-10 15:00:52 -0400

    Create Clase 2 ejercicio 6.1.py

---
+++ Clase 2 ejercicio 6.1.py
@@ -0,0 +1,20 @@
+#Diagrama de flujos de Sheldon Cooper
+Hacer llamada telefónica a la casa del sujeto cual se busca establecer lazos de amistad
+- 'si' contesta
+  -'entonces' preguntar ¿quiseras compartir una merienda conmigo?
+     -'si' la respuesta es si
+        -'entonces' cenar juntos
+        - 'y' empezar amistad
+     -'si' la respuesta es no
+        - 'entonces' ¿disfrutarias de una bebida caliente?
+           -si la respuesta es si
+           -'entonces' caso: Té 'entonces' tomar té
+                             Café 'entonces' tomar café
+                             Cocoa 'entonces' tomar cocoa
+           -'entonces' comenzar amistad
+           -si la respuesta es 'no'
+                -'entonces' rebatir: alguna actividad recreacional dile uno de tus intereses
+                           -'si' comparto mismo interes
+                              -'entonces' preguntar ¿por que no hacemos eso juntos?
+                              -'entonces' compartir intereses
+                           -'si' no comparto mismo interes
