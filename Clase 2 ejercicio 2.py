9e209b7fde1dbfe3a960ce809b2bddbcf506692e
felipetoro04 <felipe.toro2020twk.cl>
2020-06-10 15:00:41 -0400

    Create Clase 2 ejercicio 2.py

---
+++ Clase 2 ejercicio 2.py
@@ -0,0 +1,11 @@
+#MAYOR:IGUAL:MENOR A 0
+numero = int(input('Ingrese número : '))
+print('*******************************************')
+if numero == 0:
+    print('El número ingresado es igual a 0')
+else:
+    if numero > 0:
+        print('El número',numero ,'ingresado es mayor a 0')
+    else:
+        print('El número',numero ,'ingresado es menor a 0')
+print('*******************************************')
